efa0e91d5d09a27b756de995ec9e8dec85bbcb53
Niko Lucinski <niko.lucinski@evosoft.com>
2019-08-21 16:08:44 +0200

    faster

--- Jazz/expandAll.js
+++ Jazz/expandAll.js
@@ -9,14 +9,30 @@
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // ==/UserScript==
 
+function delayedCheck() {
+  if (
+    document.querySelector(
+      "#com_ibm_team_rtc_foundation_web_ui_widgets_FlatButton_8 > div > a"
+    ) === null
+  ) {
+    console.log("nicht da");
+    setTimeout(delayedCheck, 100);
+  } else {
+    console.log("da");
+    setTimeout(clickExpand, 500);
+  }
+}
+
+function clickExpand() {
+  document
+    .querySelector(
+      "#com_ibm_team_rtc_foundation_web_ui_widgets_FlatButton_8 > div > a"
+    )
+    .click();
+}
+
 jQuery(function() {
-  setTimeout(function() {
-    document
-      .querySelector(
-        "#com_ibm_team_rtc_foundation_web_ui_widgets_FlatButton_8 > div > a"
-      )
-      .click();
-  }, 3000);
+  delayedCheck();
 });
 
 // fix: script wouldnt load when pressing BACK
