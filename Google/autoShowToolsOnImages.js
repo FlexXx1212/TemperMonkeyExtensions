14903b501f4a509e3cb5b6608d8621733d5f1a17
Niko Lucinski <niko.lucinski@evosoft.com>
2019-08-21 16:09:33 +0200

    removed unused import

--- Google/autoShowToolsOnImages.js
+++ Google/autoShowToolsOnImages.js
@@ -9,7 +9,6 @@
 // @include        https://www.google.*
 // @include        https://google.*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 jQuery(function() {
