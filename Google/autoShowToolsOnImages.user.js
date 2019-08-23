496bfde32e0a23a8c2549d9c377e7b6feb2eec10
Niko Lucinski <niko.lucinski@evosoft.com>
2019-08-23 11:17:01 +0200

    best version yet

--- Google/autoShowToolsOnImages.user.js
+++ Google/autoShowToolsOnImages.user.js
@@ -1,19 +1,22 @@
 // ==UserScript==
 // @name           Google Automatically Show Tools
 // @namespace      Flex
-// @version        1.2
+// @version        1.4
 // @description    More functionality
 // @author         FlexNiko
-// @include        http://www.google.*&tbm=isch*
-// @include        http://google.**&tbm=isch*
-// @include        https://www.google.**&tbm=isch*
-// @include        https://google.**&tbm=isch*
+// @include        http://www.google.*/search?*
+// @include        http://google.*/search?*
+// @include        https://www.google.*/search?*
+// @include        https://google.*/search?*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
 
 jQuery(function() {
   var elem = document.getElementById("hdtbMenus");
   elem.className = "hdtb-td-o";
+
+  elem = document.getElementById("appbar");
+  elem.classList.add("hdtb-ab-o");
 });
 
 // fix: script wouldnt load when pressing BACK
