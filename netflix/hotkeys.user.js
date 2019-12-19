a2824e3be9d45be55974f0cdf0ba3c03daa65877
Niko Lucinski <niko.lucinski@evosoft.com>
2019-12-19 13:22:36 +0100

    new brightness adjust

--- netflix/hotkeys.user.js
+++ netflix/hotkeys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Netflix Hotkeys
 // @namespace      Flex
-// @version        1.0
+// @version        1.1
 // @description    Hotkeys
 // @author         FlexNiko
 // @include        http://www.netflix.com/*
@@ -31,4 +31,41 @@ document.onkeyup = function(e) {
   else if (e.which == 68) {
     player.seek(player.getCurrentTime() + 10000);
   }
+  // brightness
+  else if (e.which == 87) {
+    if (getBrightness() === "1") {
+      setBrightness(2.5);
+    } else {
+      setBrightness(1);
+    }
+  }
 };
+var html = document.getElementsByTagName("html")[0];
+
+function getBrightness() {
+  return html.getAttribute("data-video-brightness") || 1;
+}
+
+function setBrightness(n) {
+  html.setAttribute("data-video-brightness", n);
+
+  var str = "brightness(" + n + ")";
+
+  var netflix = document.getElementById("netflix-player");
+  if (netflix) {
+    netflix.style["-webkit-filter"] = str;
+  } else {
+    var videos = document.getElementsByTagName("video");
+    if (videos) {
+      for (var i = 0; i < videos.length; i++) {
+        videos[i].style["-webkit-filter"] = str;
+      }
+    }
+    var objects = document.getElementsByTagName("object");
+    if (objects) {
+      for (var i = 0; i < objects.length; i++) {
+        objects[i].style["-webkit-filter"] = str;
+      }
+    }
+  }
+}
