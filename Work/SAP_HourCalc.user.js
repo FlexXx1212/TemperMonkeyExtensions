2150e1024aef64906f3104d34e01d69d9037c1f3
FlexXx1212 <33630274+FlexXx1212@users.noreply.github.com>
2022-11-22 15:44:35 +0100

    Update SAP_HourCalc.user.js

--- Work/SAP_HourCalc.user.js
+++ Work/SAP_HourCalc.user.js
@@ -4,7 +4,7 @@
 // @version        1.0
 // @description    Adds a Display of Current Daily hours, and hour diff
 // @author         FlexNiko
-// @include        https://hr-selfservice.intra.men.de:44309/sap/bc/ui5_ui5/ui2/ushell/shells/abap/*
+// @include        https://hr-selfservice.intra.men.de:44309/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant          GM_addStyle
@@ -15,9 +15,6 @@ if(window.location.href.includes("#TimeEntry-change")) {
     waitForKeyElements("#__toolbar0", createDisplay);
 }
 
-if(window.location.href.includes("#TimeEntry-displayTimestatement")) {
-    waitForKeyElements("#__toolbar0", overtimeDisplay);
-}
 var currentSeconds = 0;
 const dailySeconds = 8 * 60 * 60;
 const pauseThreshold = 6 * 60 * 60;
@@ -25,49 +22,6 @@ const pauseSeconds = 30 * 60;
 var subtractPause = true;
 
 var currentHoursElement, remainingHoursElement, pauseBookedElement;
-
-function handleNegativeEntries(){
-  var foundEntries = $(".sapMObjectNumberStatusError");
-  foundEntries.each(function(index) {
-      this.parentElement.nextElementSibling.firstChild.classList.add("sapMObjectNumberStatusError");
-      var percentage = parseFloat(this.firstChild.innerText.replace("-",""));
-      var totalHours = parseFloat(this.parentElement.previousElementSibling.previousElementSibling.firstChild.firstChild.innerText);
-      this.parentElement.nextElementSibling.firstChild.firstChild.innerText = "-" + (totalHours * percentage / 100).toFixed(2);
-   });
-}
-
-function handleTotals() {
-  var rows = $(".sapMListTblRow");
-  var lastRow = rows.splice(-1)[0];
-  var newRow = lastRow.cloneNode(true);
-  newRow.id = "totalHours";
-  newRow.setAttribute("data-sap-ui","totalhours");
-  lastRow.parentElement.appendChild(newRow);
-
-  for(var i = 0; i < newRow.children.length; i++) {
-    if(i != 5 && i != 0 && i != 9) {
-      newRow.children[i].firstChild.remove();
-    }
-  }
-
-  var overtime = $("div[id*='cellOverTime']");
-  var overtimeTotal = 0.00;
-  overtime.each(function(index) {
-    if(index != overtime.length-1) {
-      overtimeTotal += parseFloat(this.firstChild.innerText);
-    } else {
-      this.firstChild.innerText = overtimeTotal.toFixed(2);
-      this.firstChild.style.fontWeight = "bolder";
-      this.firstChild.style.fontSize = "18px";
-    }
-  });
-}
-
-function overtimeDisplay() {
-    handleNegativeEntries();
-    handleTotals();
-}
-
 
 function calcHours() {
    var timeElements = $(".sapMObjectIdentifierText");
