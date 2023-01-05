d4eeeb921f163564f679e18583cbdc49481f20b6
FlexXx1212 <33630274+FlexXx1212@users.noreply.github.com>
2023-01-05 12:31:16 +0100

    Update SAP_RemoveSmoking.user.js

--- Work/SAP_RemoveSmoking.user.js
+++ Work/SAP_RemoveSmoking.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           SAP - Remove Smoking
 // @namespace      Flex
-// @version        1.0
+// @version        1.1
 // @description    remove all smoking
 // @author         FlexNiko
 // @include        https://hr-selfservice.intra.men.de:44309/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
@@ -9,9 +9,11 @@
 // @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-if(window.location.href == "https://hr-selfservice.intra.men.de:44309/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#LeaveRequest-manage") {
-    waitForKeyElements(".sapMLIB.sapMLIB-CTX.sapMLIBShowSeparator.sapMLIBTypeNavigation.sapMLIBActionable.sapMLIBHoverable.sapMLIBFocusable.sapMListTblRow", removeSmoking);
-}
+//if(window.location.href == "https://hr-selfservice.intra.men.de:44309/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#LeaveRequest-manage") {
+//waitForKeyElements('#application-LeaveRequest-manage-component---overview--leaveRequestTable', removeSmoking);
+waitForKeyElements('[id^=application-LeaveRequest-manage-component---overview--leaveRequestColumnListItem-application-LeaveRequest-manage-component---overview--leaveRequestTable-', removeSmoking);
+
+//}
 function removeSmoking() {
     var row = $(".sapMLIB.sapMLIB-CTX.sapMLIBShowSeparator.sapMLIBTypeNavigation.sapMLIBActionable.sapMLIBHoverable.sapMLIBFocusable.sapMListTblRow");
     for(var i = 0; i < row.length; i++) {
