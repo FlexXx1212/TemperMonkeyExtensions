0c9af8ab2dca659848e3e796d063a41810ea88de
FlexXx1212 <33630274+FlexXx1212@users.noreply.github.com>
2024-10-22 17:39:44 +0200

    Update video-hotkeys.user.js

--- General/video-hotkeys.user.js
+++ General/video-hotkeys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Video Hotkeys
 // @namespace      Flex
-// @version        2.1
+// @version        2.2
 // @description    Play Skip Rewind Pause for Youtube or any other Websites on Keys : A S D & Unlimited Playback Rates (x2.5 etc.)
 // @author         FlexNiko
 // @include        *
@@ -48,6 +48,7 @@ function doc_keyUp(e) {
   if (e.keyCode == 68) {
     video.currentTime += 5;
   }
+  let showCurrentSpeed = false;
   if (e.keyCode == 190 && e.shiftKey == true) {
     if(lastPlaybackRate == -1) {
         lastPlaybackRate = video.playbackRate;
@@ -62,6 +63,7 @@ function doc_keyUp(e) {
         }
         lastPlaybackRate = video.playbackRate;
     }
+    showCurrentSpeed = true;
   }
   if (e.keyCode == 188 && e.shiftKey == true) {
       if(lastPlaybackRate == -1) {
@@ -77,6 +79,7 @@ function doc_keyUp(e) {
           }
           lastPlaybackRate = video.playbackRate;
       }
+      showCurrentSpeed = true;
   }
   if (e.keyCode == 83) {
     if (video.paused) {
@@ -85,5 +88,51 @@ function doc_keyUp(e) {
       video.pause();
     }
   }
+
+  if(showCurrentSpeed && !window.location.href.includes("youtube.com")) {
+      var wrapper = document.getElementById('showSpeedWrapper');
+      if (wrapper) {
+          wrapper.remove();
+      }
+
+      let bezelWrapper = document.createElement('div');
+      bezelWrapper.id = "showSpeedWrapper";
+      bezelWrapper.className = 'ytp-bezel-text-wrapper';
+
+      // Create the inner div with class 'ytp-bezel-text'
+      let bezelText = document.createElement('div');
+      bezelText.className = 'ytp-bezel-text';
+      bezelText.textContent = lastPlaybackRate+ 'x'; // Set the inner text
+
+      // Append the inner div to the outer div
+      bezelWrapper.appendChild(bezelText);
+
+      video.parentElement.prepend(bezelWrapper);
+      setTimeout(function(){bezelWrapper.remove();}, 1000)
+
+  }
 }
 document.addEventListener("keyup", doc_keyUp, false);
+
+
+GM_addStyle(`
+.ytp-bezel-text-wrapper {
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 10%;
+    z-index: 19;
+}
+
+.ytp-bezel-text {
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 175%;
+    background: rgba(0, 0, 0, .5);
+    pointer-events: none;
+    border-radius: 3px;
+    color: white !important;
+}
+
+`);
