306b9eba31c6839a71d5b666a67b4d19b6455736
FlexXx1212 <33630274+FlexXx1212@users.noreply.github.com>
2024-11-05 23:39:54 +0100

    hotkeyless ahk and rate syncing

--- General/video-hotkeys.user.js
+++ General/video-hotkeys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Video Hotkeys
 // @namespace      Flex
-// @version        2.2
+// @version        2.3
 // @description    Play Skip Rewind Pause for Youtube or any other Websites on Keys : A S D & Unlimited Playback Rates (x2.5 etc.)
 // @author         FlexNiko
 // @include        *
@@ -10,12 +10,21 @@
 // @grant          GM_getValue
 // ==/UserScript==
 
-var lastPlaybackRate = -1;
 var originalTitle = "";
+function getRate() {
+    if(localStorage.getItem("lastPlaybackRate") != null) {
+        return JSON.parse(localStorage.getItem("lastPlaybackRate"));
+    } else {
+        return -1;
+    }
+}
+
+function setRate(rate) {
+    localStorage.setItem("lastPlaybackRate", JSON.stringify(rate));
+}
+
 function doc_keyUp(e) {
-  if(originalTitle == "") {
-      originalTitle = document.getElementsByTagName("title")[0]?.innerText;
-  }
+  console.log(getRate());
   if (
     e.target.tagName.toLowerCase() == "textarea" ||
     e.target.id === "search" ||
@@ -23,7 +32,9 @@ function doc_keyUp(e) {
     e.target.className === "comment-simplebox-text" ||
     e.target.tagName.toLowerCase() === "yt-formatted-string" ||
     e.target.role === "textbox" ||
-    e.target.type === "text"
+    e.target.type === "text" ||
+    e.target.type === "input" ||
+    e.target.type === "search"
   ) {
     return;
   }
@@ -48,68 +59,49 @@ function doc_keyUp(e) {
   if (e.keyCode == 68) {
     video.currentTime += 5;
   }
-  let showCurrentSpeed = false;
-  if (e.keyCode == 190 && e.shiftKey == true) {
-    if(lastPlaybackRate == -1) {
-        lastPlaybackRate = video.playbackRate;
-    } else {
-        video.playbackRate = lastPlaybackRate + 0.25;
-        var b = document.getElementsByClassName("ytp-bezel-text");
-        if(b != null && b.length > 0) {
-            b[0].innerText = video.playbackRate + "x";
-        }
-        if(document.getElementsByTagName("title")[0] != null) {
-          document.getElementsByTagName("title")[0].innerText = video.playbackRate + "x - " + originalTitle;
-        }
-        lastPlaybackRate = video.playbackRate;
-    }
-    showCurrentSpeed = true;
-  }
-  if (e.keyCode == 188 && e.shiftKey == true) {
-      if(lastPlaybackRate == -1) {
-          lastPlaybackRate = video.playbackRate;
-      } else if(lastPlaybackRate > 0.25) {
-          video.playbackRate = lastPlaybackRate - 0.25;
-          var c = document.getElementsByClassName("ytp-bezel-text");
-          if(c != null && c.length > 0) {
-            c[0].innerText = video.playbackRate + "x";
-          }
-          if(document.getElementsByTagName("title")[0] != null) {
-            document.getElementsByTagName("title")[0].innerText = video.playbackRate + "x - " + originalTitle;
-          }
-          lastPlaybackRate = video.playbackRate;
+  if (e.shiftKey == true && (e.keyCode == 190 || e.keyCode == 188)) {
+      if(getRate() == -1) {
+          setRate(video.playbackRate);
       }
-      showCurrentSpeed = true;
+      if (e.keyCode == 190) {
+          video.playbackRate = getRate() + 0.25;
+      }
+      if (e.keyCode == 188) {
+          video.playbackRate = getRate() - 0.25;
+      }
+      var b = document.getElementsByClassName("ytp-bezel-text");
+      if(b != null && b.length > 0) {
+          b[0].innerText = video.playbackRate + "x";
+      }
+      setRate(video.playbackRate);
+      if(!window.location.href.includes("youtube.com")) {
+          var wrapper = document.getElementById('showSpeedWrapper');
+          if (wrapper) {
+              wrapper.remove();
+          }
+
+          let bezelWrapper = document.createElement('div');
+          bezelWrapper.id = "showSpeedWrapper";
+          bezelWrapper.className = 'ytp-bezel-text-wrapper';
+
+          // Create the inner div with class 'ytp-bezel-text'
+          let bezelText = document.createElement('div');
+          bezelText.className = 'ytp-bezel-text';
+          bezelText.textContent = getRate() + 'x'; // Set the inner text
+
+          // Append the inner div to the outer div
+          bezelWrapper.appendChild(bezelText);
+
+          video.parentElement.prepend(bezelWrapper);
+          setTimeout(function(){bezelWrapper.remove();}, 1000)
+      }
   }
   if (e.keyCode == 83) {
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  }
-
-  if(showCurrentSpeed && !window.location.href.includes("youtube.com")) {
-      var wrapper = document.getElementById('showSpeedWrapper');
-      if (wrapper) {
-          wrapper.remove();
+      if (video.paused) {
+          video.play();
+      } else {
+          video.pause();
       }
-
-      let bezelWrapper = document.createElement('div');
-      bezelWrapper.id = "showSpeedWrapper";
-      bezelWrapper.className = 'ytp-bezel-text-wrapper';
-
-      // Create the inner div with class 'ytp-bezel-text'
-      let bezelText = document.createElement('div');
-      bezelText.className = 'ytp-bezel-text';
-      bezelText.textContent = lastPlaybackRate+ 'x'; // Set the inner text
-
-      // Append the inner div to the outer div
-      bezelWrapper.appendChild(bezelText);
-
-      video.parentElement.prepend(bezelWrapper);
-      setTimeout(function(){bezelWrapper.remove();}, 1000)
-
   }
 }
 document.addEventListener("keyup", doc_keyUp, false);
